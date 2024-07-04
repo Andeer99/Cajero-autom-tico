39669602e2e7a708365610f6d9921821bed83d97
ander <anderag062@gmail.com>
2024-07-04 09:04:21 -0600

    Comienzo desde lo visto en la clase de Javascript DEV.F

---
+++ main.js
@@ -0,0 +1,27 @@
+function ingresar(event){
+    let username = document.getElementById("username");
+    let password = document.getElementById("password").value;
+    let mensaje = document.getElementById("mensaje");
+
+    if(username.value === "ander@gmail.com" && password === "1234"){
+        window.location.href = "inicio.html";
+        //Pendiente, inhabilitar boton de ingresar 
+    } else{
+        mensaje.style.color = "red";
+        mensaje.innerHTML = "Error al iniciar sesión";
+    }
+}
+let usuarios = [
+    {
+    user: "Antonio",
+    pass: "1234"
+    },
+    {
+    user: "Raul",
+    pass: "12345"
+    },
+    {
+    user: "Panarabit",
+    pass: "123456"
+    }
+]
